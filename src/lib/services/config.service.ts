a68e5a6199f0d93b66ac14e4e01f7da37ed7f5d0
Diego SanMo <diego14290764@itcg.edu.mx>
2020-07-14 15:13:51 -0500

    Removing some decorator and updating the package.json

--- src/lib/services/config.service.ts
+++ src/lib/services/config.service.ts
@@ -1,18 +1,17 @@
 import ConfigInterface from '../interfaces/config.interface';
 import { Injectable } from '@angular/core';
 
-@Injectable()
 export class ConfigService {
   space_id: string = '';
   environment: string = '';
   delivery_accessToken: string = '';
 
-  constructor( data: ConfigInterface) {
+  constructor(private data: ConfigInterface) {
 
-    if (data) {
-      this.space_id = data.space_id;
-      this.environment = data.environment;
-      this.delivery_accessToken = data.delivery_accessToken;
+    if (this.data) {
+      this.space_id = this.data.space_id;
+      this.environment = this.data.environment;
+      this.delivery_accessToken = this.data.delivery_accessToken;
 
     }
   }
